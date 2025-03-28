7ae95775e8dd254dfcf9279ba9d4c1b4f77cc847
Eric Steele <eric.steele@ezcater.com>
2025-03-28 13:06:24 -0400

    Show symlinks when finding config files

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,5 +5,15 @@ return {
   },
   keys = {
     { "<leader><space>", LazyVim.pick("smart"), desc = "Find Files (Smart)" },
+    {
+      "<leader>fc",
+      function()
+        Snacks.picker.files({
+          cwd = vim.fn.stdpath("config"),
+          follow = true,
+        })
+      end,
+      desc = "Find Config Files",
+    },
   },
 }
