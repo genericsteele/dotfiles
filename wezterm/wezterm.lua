b0f4a4fbc4287a57ac72a0edbfab4cec2f6ef2cc
Eric Steele <eric.steele@ezcater.com>
2025-04-03 09:18:43 -0400

    Add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Font Settings
+
+config.font = wezterm.font("DankMono Nerd Font")
+config.font_size = 18.0
+
+-- Color Scheme
+
+config.color_scheme = "Tokyo Night"
+
+-- Window Stuff
+
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.initial_cols = 240
+config.initial_rows = 72
+
+-- Misc
+
+config.prefer_egl = true
+config.default_cwd = "/Users/ericsteele/code/ezcater/ez-rails/"
+
+return config
