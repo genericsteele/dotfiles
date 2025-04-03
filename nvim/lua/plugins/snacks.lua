b33f5e4750ad4ebde6f207f18b98a2def27ea2d3
Eric Steele <eric.steele@ezcater.com>
2025-04-03 09:19:37 -0400

    Add dotfiles picker (<leader>fd)

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -6,6 +6,16 @@ return {
   keys = {
     { "<leader><space>", LazyVim.pick("smart"), desc = "Find Files (Smart)" },
     {
+      "<leader>fd",
+      function()
+        Snacks.picker.files({
+          cwd = vim.fn.expand("$HOME/code/dotfiles"),
+          follow = true,
+        })
+      end,
+      desc = "Find dotfiles",
+    },
+    {
       "<leader>fc",
       function()
         Snacks.picker.files({
